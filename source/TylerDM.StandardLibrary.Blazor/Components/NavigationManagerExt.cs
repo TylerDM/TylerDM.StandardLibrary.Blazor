ad14900c62e80b6e56e5120c74efa0b2f97ab0d9
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-07 11:11:33 -0400

    Commit for easy machine switch.

---
+++ source/TylerDM.StandardLibrary.Blazor/Components/NavigationManagerExt.cs
@@ -0,0 +1,35 @@
+﻿namespace TylerDM.StandardLibrary.Blazor.Components;
+
+public static class NavigationManagerExt
+{
+	#region methods
+	public static string GetParameter(this NavigationManager navigationManager, string key)
+	{
+		if (string.IsNullOrEmpty(key)) return string.Empty;
+
+		var query = HttpUtility.ParseQueryString(new Uri(navigationManager.Uri).Query);
+		var values = query.GetValues(key) ?? [];
+		if (values.Length is 0) return string.Empty;
+		return values[0];
+	}
+
+	public static int GetIntParameter(this NavigationManager navigationManager, string key)
+	{
+		var value = navigationManager.GetParameter(key);
+		if (string.IsNullOrWhiteSpace(value)) return default;
+
+		return int.Parse(value);
+	}
+
+	public static Guid GetGuidParameter(this NavigationManager navigationManager, string key)
+	{
+		var value = navigationManager.GetParameter(key);
+		if (string.IsNullOrWhiteSpace(value)) return default;
+
+		return Guid.Parse(value);
+	}
+
+	//public static void NavigateTo(this NavigationManager navigationManager, string endpoint, params IWebParameter[] parameters) =>
+	//	navigationManager.NavigateTo(UrlFactory.Create(endpoint, parameters));
+	#endregion
+}
